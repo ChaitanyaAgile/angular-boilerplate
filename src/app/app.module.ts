23a73f43ddd7ac8053d8201cfdee7d07df89e59f
chaitanya.tank <chaitanya.p@agileinfoways.com>
2023-08-10 11:49:24 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layout/header/header.component';
+import { MaterialModule } from './material.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ErrorInterceptor } from './interceptor/error-interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,49 @@
+import { Router } from '@angular/router';
+import { AuthService } from '../../authComponent/service/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { responseModel } from '../../model/reponseModel';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  showLogout = false;
+  userToken: any;
+  constructor(private authService: AuthService,
+    private router: Router) {
+      /**
+       * check that use is login or not base on that we will show hide login logout button
+       */
+    this.authService.isLogin.subscribe((res) => {
+      this.userToken = localStorage.getItem('access-token');
+      if (this.userToken) {
+        this.showLogout = true;
+      } else {
+        this.showLogout = res;
+      }
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * Logut api will call and if api goes successfull it will clear localstorage and return t login page
+   */
+  logout() {
+    this.authService.logout(this.userToken).subscribe((res: responseModel) => {
+    // to clear locaslstorage value and clear token and permission use this method
+    this.authService.clearLocalStorage();
+    
+      this.router.navigateByUrl('/login');
+    });
+    // to clear locaslstorage value and clear token and permission use this method
+    this.authService.clearLocalStorage();
+    this.authService.setisLoginValue(false);
+    this.router.navigateByUrl('/login');
+  }
+
+}
